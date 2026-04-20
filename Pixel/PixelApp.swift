787b2016ac2a3e90a6198af5ce980cf07e663e6e
yoshirozay <66914659+yoshirozay@users.noreply.github.com>
2026-04-20 16:21:15 -0400

    Initial Commit

---
+++ Pixel/PixelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelApp.swift
+//  Pixel
+//
+//  Created by Carson O'Sullivan on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct PixelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
